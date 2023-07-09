3928ec4777f113d0ce8617a285c64f0aefa75f7c
Bponthemove <bpvanzalk@hotmail.com>
2023-07-09 19:51:25 +0100

    vite too big warning

--- vite.conifg.ts
+++ vite.conifg.ts
@@ -8,6 +8,17 @@ export default defineConfig({
     jsxFactory: 'React.createElement',
     jsxFragment: 'React.Fragment',
   },
+  build: {
+    rollupOptions: {
+        output:{
+            manualChunks(id) {
+                if (id.includes('node_modules')) {
+                    return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                }
+            }
+        }
+    }
+},
   css: {
     modules: {
       localsConvention: 'camelCaseOnly',
