aaa9a34704d079ae194c58abba3f49c55b20dbaa
Bponthemove <bpvanzalk@hotmail.com>
2024-06-17 11:34:09 +0100

    stripe not happy discount only when available

--- netlify/functions/stripePayCart.ts
+++ netlify/functions/stripePayCart.ts
@@ -8,7 +8,7 @@ const handler = async (event: HandlerEvent, context: HandlerContext) => {
   let status: number;
   if (checkOut.items) {
     const { email, couponId, items } = checkOut;
-    console.log({ email, couponId });
+
     const modifiedLineItems = items.map(
       (item: { quantity: number; id: string; price: number }) => ({
         price_data: {
@@ -19,35 +19,45 @@ const handler = async (event: HandlerEvent, context: HandlerContext) => {
         quantity: item.quantity,
       })
     );
-    try {
-      session = await stripe.checkout.sessions.create({
-        mode: "payment",
-        line_items: modifiedLineItems,
-        discounts: [
-          {
-            // add coupon to Stripe dashboard, add coupon id to user in db
-            coupon: couponId ?? null,
-          },
-        ],
-        shipping_options: [
-          {
-            shipping_rate_data: {
-              type: "fixed_amount",
-              display_name: checkOut.shipping
-                ? "Delivery cost"
-                : " Free Delivery",
-              fixed_amount: {
-                amount: checkOut.shipping ? 399 : 0,
-                currency: "gbp",
-              },
+
+    let checkoutObject = {
+      mode: "payment",
+      line_items: modifiedLineItems,
+      shipping_options: [
+        {
+          shipping_rate_data: {
+            type: "fixed_amount",
+            display_name: checkOut.shipping
+              ? "Delivery cost"
+              : " Free Delivery",
+            fixed_amount: {
+              amount: checkOut.shipping ? 399 : 0,
+              currency: "gbp",
             },
           },
+        },
+      ],
+      success_url: "https://roundthefield.co.uk/afterstripe/success",
+      // cancel_url: "https://roundthefield.co.uk/cart",
+      cancel_url: "https://roundthefield.co.uk//afterstripe/failed",
+      customer_email: email,
+    };
+
+    if (couponId) {
+      // add coupon to Stripe dashboard, add coupon id to user in db
+      checkoutObject = {
+        ...checkoutObject,
+        // @ts-ignore: Unreachable code error
+        discounts: [
+          {
+            coupon: couponId,
+          },
         ],
-        success_url: "https://roundthefield.co.uk/afterstripe/success",
-        // cancel_url: "https://roundthefield.co.uk/cart",
-        cancel_url: "https://roundthefield.co.uk//afterstripe/failed",
-        customer_email: email,
-      });
+      };
+    }
+
+    try {
+      session = await stripe.checkout.sessions.create(checkoutObject);
       status = 200;
     } catch (err) {
       console.error(err);
